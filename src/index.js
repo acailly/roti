7a4de95c704b536572b8561cf650b939fc0e241b
acailly <ylliac14@gmail.com>
2018-01-28 08:06:26 +0100

    :construction: Updated dependencies version

--- src/index.js
+++ src/index.js
@@ -1,73 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-import IPFS from 'ipfs'
-import Y from 'yjs/dist/y'
-import yMemory from 'y-memory/dist/y-memory'
-import yIpfs from 'y-ipfs-connector'
-import yArray from 'y-array/dist/y-array'
-import yMap from 'y-map/dist/y-map'
+import IPFS from "ipfs";
+import Y from "yjs";
+import yMemory from "y-memory";
+import yIpfs from "y-ipfs-connector";
+import yArray from "y-array";
+import yMap from "y-map";
 
-yMemory(Y)
-yIpfs(Y)
-yArray(Y)
-yMap(Y)
+yMemory(Y);
+yIpfs(Y);
+yArray(Y);
+yMap(Y);
 
-
-function repo () {
-return 'ipfs/acailly-yjs-demo/' + Math.random()
+function repo() {
+  return "ipfs/acailly-yjs-demo/" + Math.random();
 }
 
 const ipfs = new IPFS({
-    repo: repo(),
-    EXPERIMENTAL: {
-        pubsub: true
+  repo: repo(),
+  EXPERIMENTAL: {
+    pubsub: true
+  }
+});
+
+ipfs.on("error", err => {
+  console.error(err);
+});
+
+ipfs.on("ready", () =>
+  ipfs.id((err, info) => {
+    if (err) {
+      throw err;
     }
-})
 
-ipfs.on('error', (err) => {
-	console.error(err)
-}) 
-
-ipfs.on('ready', () => ipfs.id((err, info) => {
-    if (err) { throw err }
-
-    console.log('IPFS node ready with address ' + info.id)
+    console.log("IPFS node ready with address " + info.id);
 
     ipfs.id((err, peerId) => {
-        if (err) { throw err }
-        
-        console.log('Peer ID is:', peerId)
+      if (err) {
+        throw err;
+      }
 
-        window.peerId = peerId
+      console.log("Peer ID is:", peerId);
 
-        Y({
-            db: {
-                name: 'memory' 
-            },
-            connector: {
-                name: 'ipfs',
-                room: 'acailly-roti',
-                ipfs: ipfs
-            },
-            share: { 
-                0: 'Array',
-                1: 'Array',
-                2: 'Array',
-                3: 'Array',
-                4: 'Array',
-                messages: 'Array'
-            }
-        }).then(function (y) {
-            window.y = y
-            console.log('Yjs instance ready!')
-            
-            ReactDOM.render(<App />, document.getElementById('root'));
-            registerServiceWorker();
-        })
-    })
-}))
+      window.peerId = peerId;
 
+      Y({
+        db: {
+          name: "memory"
+        },
+        connector: {
+          name: "ipfs",
+          room: "acailly-roti",
+          ipfs: ipfs
+        },
+        share: {
+          0: "Array",
+          1: "Array",
+          2: "Array",
+          3: "Array",
+          4: "Array",
+          messages: "Array"
+        }
+      }).then(function(y) {
+        window.y = y;
+        console.log("Yjs instance ready!");
 
+        ReactDOM.render(<App />, document.getElementById("root"));
+        registerServiceWorker();
+      });
+    });
+  })
+);
